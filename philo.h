be5e1003c3b8dbe3eac7e1fd464cffd864dd6b23
yhebbat <58018346+yhebbat@users.noreply.github.com>
2021-09-18 19:28:02 +0100

    add

--- philo.h
+++ philo.h
@@ -3,26 +3,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+# define FORK 1
+# define EAT 2
+# define SLEEP 3
+# define THINK 4
+# define DIE 5
+
 typedef	struct	s_args
 {
-	long	number_philo;
-	long	time_die;
-	long	time_eat;
-	long	time_sleep;
-	long	num_meal;
-	// int		done;
-	pthread_mutex_t *forks;
-	pthread_mutex_t write;
-	struct t_philo *philo;
+	long				number_philo;
+	long				time_die;
+	long				time_eat;
+	long				time_sleep;
+	long				num_meal;
+	unsigned long long	current_time;
+	pthread_mutex_t 	*forks;
+	pthread_mutex_t 	write;
+	pthread_mutex_t		is_eating;
+	struct t_philo 		*philo;
 }t_args;
 
 typedef struct s_philo
 {
-	pthread_t id;
-	int	philo_id;
-	// pthread_mutex_t eat;
-	unsigned long long last_time_eat;
-	int is_eating;
-	int nb_of_meals;
-	t_args *args;
+	pthread_t			tr;
+	int					philo_id;
+	unsigned long long	last_time_eat;
+	int					nb_of_meals;
+	int					eat;
+	t_args				*args;
 }t_philo;
